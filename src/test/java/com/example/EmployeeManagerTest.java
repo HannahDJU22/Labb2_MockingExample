2a28c0904332341a65ae92f6c37f856698266b9e
hannah <hannah.degerheim@iths.se>
2023-02-06 09:29:47 +0100

    test fixed for payemployees

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,4 +1,46 @@
 package com.example;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class EmployeeManagerTest {
+
+    public BankService bankServiceMock;
+    public EmployeeRepository employeeRepositoryMock;
+    public EmployeeManager employeeManager;
+    public List<Employee> employees = new ArrayList<>();
+
+    @BeforeAll
+
+    public void setUp(){
+        bankServiceMock= mock(BankService.class);
+        employeeRepositoryMock = mock(EmployeeRepository.class);
+        employeeManager = new EmployeeManager(employeeRepositoryMock, bankServiceMock);
+    }
+    @Test
+    public void testPayEmployees(){
+        employees.add(new Employee("1", 200));
+        employees.add(new Employee("2", 200));
+        employees.add(new Employee("77", 40000));
+        employees.add(new Employee("46", 55000));
+
+        when(employeeRepositoryMock.findAll()).thenReturn(employees);
+
+        assertEquals(4, employeeManager.payEmployees());
+
+    }
+
+
 }
