9e0e7b3b7d61915377d2d24e66f6d8c895e064a2
hannah <hannah.degerheim@iths.se>
2023-02-07 13:43:43 +0100

    created test with TDD for adding numbers

--- src/test/java/com/example/TDDCalculatorTest.java
+++ src/test/java/com/example/TDDCalculatorTest.java
@@ -1,15 +1,15 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TDDCalculatorTest {
-    int expected = 10;
 
+    @Test
+    public void testAddingTwoNumbers(){
+        TDDCalc calc = new TDDCalc();
 
-@Test
-    void addingTwoNumbersTest(){
-        assertEquals(expected, addingTwoNumbers(a, b));
+    assertEquals(10, calc.addingTwoNumbers(6, 4));
+
     }
 }
