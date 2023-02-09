60091da05c371f38972031354ee7292ee5c177fc
hannah <hannah.degerheim@iths.se>
2023-02-09 09:59:42 +0100

    added test for adding numbers in list

--- src/test/java/com/example/TDDCalculatorTest.java
+++ src/test/java/com/example/TDDCalculatorTest.java
@@ -1,6 +1,10 @@
 package com.example;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TDDCalculatorTest {
@@ -20,4 +24,14 @@ public class TDDCalculatorTest {
     public void AddingSeveralNUmbersShouldWork(){
         assertEquals(20, calc.add(5, 5, 3, 7));
     }
+
+    @Test
+    public void AddingListNumbersShouldWork(){
+        assertEquals(20, calc.add(Lists.list(5, 5, 3, 7)));
+    }
+
+
+
 }
+
+
