5628145d589580a7cea274a41bfe988170e19a44
hannah <hannah.degerheim@iths.se>
2023-02-09 10:17:41 +0100

    added test for adding numbers in empty list
    Also added/updated the method in calc class to match new tests for list

--- src/main/java/com/example/TDDCalc.java
+++ src/main/java/com/example/TDDCalc.java
@@ -1,11 +1,21 @@
 package com.example;
 
+import java.util.List;
+
 public class TDDCalc {
 
-
-    public int add(int a, int b, int... others) {
+//keeping old method for me to remember
+/*    public int add(int a, int b, int... others) {
         int addingNumbers = a+b;
         for (int i:others) {
+            addingNumbers +=i;
+        }
+        return addingNumbers;
+    }*/
+
+    public int add(List<Integer> numbers){
+        Integer addingNumbers = 0;
+        for (int i:numbers) {
             addingNumbers +=i;
         }
         return addingNumbers;
--- src/test/java/com/example/TDDCalculatorTest.java
+++ src/test/java/com/example/TDDCalculatorTest.java
@@ -10,24 +10,28 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TDDCalculatorTest {
     TDDCalc calc = new TDDCalc();
 
-    @Test
+/*    @Test
     public void AddingNumbersShouldWork(){
         assertEquals(10, calc.add(5, 5));
-    }
+    }*/
 
 /*   @Test
     public void AddingNumbersWithIncorrectTypeSHouldFail(){
         assertEquals(10, calc.add(6, 4L));
     }*/
 
-    @Test
+/*    @Test
     public void AddingSeveralNUmbersShouldWork(){
         assertEquals(20, calc.add(5, 5, 3, 7));
-    }
+    }*/
 
     @Test
     public void AddingListNumbersShouldWork(){
         assertEquals(20, calc.add(Lists.list(5, 5, 3, 7)));
+    }
+    @Test
+    public void AddingListNumbersFromEmptyListShouldReturnZero(){
+        assertEquals(0, calc.add(new ArrayList<>()));
     }
 
 
